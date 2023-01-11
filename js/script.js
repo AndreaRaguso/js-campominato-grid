7f4cf867bbefc85696d06dde5f5116fd1a032fa7
Andrea Raguso <info@andrearaguso.com>
2023-01-11 15:19:31 +0100

    fix identazione

--- js/script.js
+++ js/script.js
@@ -12,108 +12,58 @@ const numeriGenerati = [];
 
 submit.onclick = function(){
 
-//     for (let i = 1; i < 100; i++) {
+    for (let i = 0; i < 100; i++) {
 
-//         const newCell = document.createElement('div');
-//         newCell.classList.add('cell');
-//         newCell.addEventListener('click',
+        const newCell = createNewCell();
+        cellContainer.append(newCell);
 
-//         function () {
-            
-//             console.log(this);
-//             console.log(this.classList);
+    }
 
-//             if(this.classList.contains('clicked')){
-//                 this.classList.remove('clicked');
-//             }
-//             else{
-//                 this.classList.add('clicked');
-//             }
+    function createNewCell() {
 
-//         }
-    
-//         );
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('click',
+        
+            function () {
+                
+                console.log(this);
+                console.log(this.classList);
 
-//         let randomNumber = getRandomNumber(1, 64);
+                if(this.classList.contains('clicked')){
+                    this.classList.remove('clicked');
+                    cell.innerHTML = "";
+                }
+                else{
+                    this.classList.add('clicked');
+                    cell.innerHTML = randomNumber;
+                }
 
-//         while(numeriGenerati.includes(randomNumber)){
-//             randomNumber = getRandomNumber(1, 64);
-//         }
-
-//         numeriGenerati.push(randomNumber);
-
-//         console.log(numeriGenerati);
-
-//         console.log(randomNumber);
-//         cell.innerHTML = randomNumber;
-//         // OPPURE (VERSIONE ALTERNATIVA)
-//         // cell.append(randomNumber);
-
-//         if(randomNumber % 2 == 0){
-//             cell.classList.add('even');
-//         }
-//         else {
-//             cell.classList.add('odd');
-//         }
-
-//         return cell;
-
-//     }
-
-// }
-
-
-for (let i = 0; i < 100; i++) {
-
-    const newCell = createNewCell();
-    cellContainer.append(newCell);
-
-}
-
-function createNewCell() {
-
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.addEventListener('click',
-    
-        function () {
-            
-            console.log(this);
-            console.log(this.classList);
-
-            if(this.classList.contains('clicked')){
-                this.classList.remove('clicked');
-                cell.innerHTML = "";
             }
-            else{
-                this.classList.add('clicked');
-                cell.innerHTML = randomNumber;
-            }
+        
+        );
 
+        let randomNumber = getRandomNumber(1, 100);
+
+        while(numeriGenerati.includes(randomNumber)){
+            randomNumber = getRandomNumber(1, 100);
         }
+
+        numeriGenerati.push(randomNumber);
+
+        console.log(numeriGenerati);
+
+        console.log(randomNumber);
+
+        if(randomNumber % 2 == 0){
+            cell.classList.add('even');
+        }
+        else {
+            cell.classList.add('odd');
+        }
+
+        return cell;
+
+    }
     
-    );
-
-    let randomNumber = getRandomNumber(1, 100);
-
-    while(numeriGenerati.includes(randomNumber)){
-        randomNumber = getRandomNumber(1, 100);
-    }
-
-    numeriGenerati.push(randomNumber);
-
-    console.log(numeriGenerati);
-
-    console.log(randomNumber);
-
-    if(randomNumber % 2 == 0){
-        cell.classList.add('even');
-    }
-    else {
-        cell.classList.add('odd');
-    }
-
-    return cell;
-
-    }
 }
